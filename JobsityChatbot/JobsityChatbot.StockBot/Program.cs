59fc6a43af13ab12870bea04d904e0d572721d24
Daniel Santil <daniel.shcf@gmail.com>
2020-11-21 09:48:53 -0400

    Initial backend/fronted projects structures added

---
+++ JobsityChatbot/JobsityChatbot.StockBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobsityChatbot.StockBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
